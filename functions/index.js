329e1cd2c3fd2b74102522e6737e7c28799a20c0
Jordan Daubinet <jordan@.naked.insure>
2022-02-07 21:32:39 +0200

    Updated function to generate public urls at upload time

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,8 @@
 const functions = require('firebase-functions');
 const admin = require("firebase-admin");
-admin.initializeApp(functions.config().firebase);
 const vision = require('@google-cloud/vision');
+
+admin.initializeApp(functions.config().firebase);
 const db = admin.firestore();
 
 exports.checkForFood = functions.storage.object().onFinalize(async (object) => {
@@ -13,14 +14,19 @@ exports.checkForFood = functions.storage.object().onFinalize(async (object) => {
   );
 
   // 2. Extract the highest classification
-  let label = result.labelAnnotations[0].description
-  let score = result.labelAnnotations[0].score
+  const label = result.labelAnnotations[0].description
+  const score = result.labelAnnotations[0].score
 
-  // 2. Store Data information
+  // 3. Generate the downloadable URL's
+  const file = admin.storage().bucket().file(`${object.name}`)
+  const signedURLs = await file.getSignedUrl({action: 'read', expires: new Date(2023, 0)})
+
+  // 4. Store Data information
   const docRef = db.collection('food').doc(object.name);
   await docRef.set({
     label,
     score,
-    url: `gs://${object.bucket}/${object.name}` 
+    url: `gs://${object.bucket}/${object.name}`,
+    download: signedURLs[0]
   });
 });
